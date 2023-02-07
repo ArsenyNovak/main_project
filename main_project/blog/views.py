7f89ab4ebe66a4a09c26af8ab9435977f4a9e571
Arseny Novak <arsenya.11@mail.ru>
2023-02-07 23:49:15 +0300

    feat: create model, home_news_list.html, first view, url. Made first migrations.

--- main_project/blog/views.py
+++ main_project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+class NewsHome(ListView):
+    model = News
+    template_name = 'blog/home_news_list.html'
